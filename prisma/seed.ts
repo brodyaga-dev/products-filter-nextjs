9a45a03c8f01b03b44236b7d840da80f2d3e09e7
harshmangalam <harshdev8218@gmail.com>
2024-02-21 17:26:54 +0530

    added seeding env

---
+++ prisma/seed.ts
@@ -0,0 +1,56 @@
+import { PrismaClient, Product } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const products = [
+  {
+    brand: "Levi's",
+    description: `ABFRL, Aditya Birla Fashion and Retail Ltd,7th Floor, Skyline Icon Business Park, 86-92, Off A.K. Road, Marol Village, Andheri (East), Mumbai, Maharashtra 400059`,
+    image: "https://m.media-amazon.com/images/I/51Yvs102CgL._SL1500_.jpg",
+    title: "Allen Solly Men's Slim Fit Shirt",
+    price: 3999,
+  },
+  {
+    brand: "Eybogler",
+    description: `Fabric: Cotton Blend ; Premium Export Quality Branded T-shirt; Unique Collection to your wardrobe casuals a hit of effortless cool with this best-looking t shirt.`,
+    image: "https://m.media-amazon.com/images/I/51byp5tQ86L._SL1000_.jpg",
+    title: "EYEBOGLER Polo Neck Half Sleeve Striped Print Casual Men Tshirt",
+    price: 256,
+  },
+  {
+    brand: "Nusyl",
+    description: `Combine Sales Pvt Ltd, Combine Sales Pvt Ltd, B-1, Radhika Kunj, L.IG Khajrana Link Road, Indore (M.P) - 452010`,
+    image: "https://m.media-amazon.com/images/I/41qpsB-njtL.jpg",
+    title: "Nusyl Girls Solid Color Tshirt (NUGCTSF001)",
+    price: 299,
+  },
+  {
+    brand: "EYEBOGLER",
+    description: `Seven Rocks International, Cheema Chowk, Ludhiana., Men Rocks Pvt Ltd, #276, near Cheema Chowk, Industrial Area- A, Ludhiana, Punjab 141003`,
+    image: "https://m.media-amazon.com/images/I/51EI1UA7FKL._SL1165_.jpg",
+    title:
+      "EYEBOGLER Women's Striped Regular fit T-Shirt (T335HS_Green M) EYEBOGLERWomentunicT-Shirt",
+    price: 182,
+  },
+  {
+    brand: "Van Heusen",
+    description: `ABFRL, ROCKET KOMMERCE LLP,NO:1/1,5TH FLOOR,VINAYAKA TOWERS,1ST CROSS,GANDHINAGAR-560009,BANGALORE,Karnataka,India`,
+    image: "https://m.media-amazon.com/images/I/51EI1UA7FKL._SL1165_.jpg",
+    title: "Van Heusen Men T-Shirt",
+    price: 1819,
+  },
+];
+async function main() {
+  await prisma.product.createMany({
+    data: products,
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
