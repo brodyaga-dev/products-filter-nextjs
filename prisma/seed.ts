3cd33441f3a2f7b93f95c601e7c04f38e4b84cec
harshmangalam <harshdev8218@gmail.com>
2024-02-22 10:33:39 +0530

    added brand and category schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,32 @@
 import { PrismaClient, Product } from "@prisma/client";
 const prisma = new PrismaClient();
 
+const brands = [
+  {
+    name: "Fire voltt",
+  },
+  {
+    name: "Van Heusen",
+  },
+  {
+    name: "Eybogler",
+  },
+  {
+    name: "Levi's",
+  },
+  {
+    name: "Nusyl",
+  },
+];
+
+const categories = [
+  {
+    name: "t-shirt",
+  },
+  {
+    name: "Smartwatch",
+  },
+];
 const products = [
   {
     brand: "Levi's",
@@ -8,15 +34,15 @@ const products = [
     image: "https://m.media-amazon.com/images/I/51Yvs102CgL._SL1500_.jpg",
     title: "Allen Solly Men's Slim Fit Shirt",
     price: 3999,
-    category: "T-shirt",
+    categories: ["t-shirt"],
   },
   {
     brand: "Eybogler",
-    description: `Fabric: Cotton Blend ; Premium Export Quality Branded T-shirt; Unique Collection to your wardrobe casuals a hit of effortless cool with this best-looking t shirt.`,
+    description: `Fabric: Cotton Blend ; Premium Export Quality Branded t-shirt; Unique Collection to your wardrobe casuals a hit of effortless cool with this best-looking t shirt.`,
     image: "https://m.media-amazon.com/images/I/51byp5tQ86L._SL1000_.jpg",
     title: "EYEBOGLER Polo Neck Half Sleeve Striped Print Casual Men Tshirt",
     price: 256,
-    category: "T-shirt",
+    categories: ["t-shirt"],
   },
   {
     brand: "Nusyl",
@@ -24,24 +50,24 @@ const products = [
     image: "https://m.media-amazon.com/images/I/41qpsB-njtL.jpg",
     title: "Nusyl Girls Solid Color Tshirt (NUGCTSF001)",
     price: 299,
-    category: "T-shirt",
+    categories: ["t-shirt"],
   },
   {
-    brand: "EYEBOGLER",
+    brand: "Eybogler",
     description: `Seven Rocks International, Cheema Chowk, Ludhiana., Men Rocks Pvt Ltd, #276, near Cheema Chowk, Industrial Area- A, Ludhiana, Punjab 141003`,
     image: "https://m.media-amazon.com/images/I/51EI1UA7FKL._SL1165_.jpg",
     title:
-      "EYEBOGLER Women's Striped Regular fit T-Shirt (T335HS_Green M) EYEBOGLERWomentunicT-Shirt",
+      "EYEBOGLER Women's Striped Regular fit t-shirt (T335HS_Green M) EYEBOGLERWomentunict-shirt",
     price: 182,
-    category: "T-shirt",
+    categories: ["t-shirt"],
   },
   {
     brand: "Van Heusen",
     description: `ABFRL, ROCKET KOMMERCE LLP,NO:1/1,5TH FLOOR,VINAYAKA TOWERS,1ST CROSS,GANDHINAGAR-560009,BANGALORE,Karnataka,India`,
     image: "https://m.media-amazon.com/images/I/51EI1UA7FKL._SL1165_.jpg",
-    title: "Van Heusen Men T-Shirt",
+    title: "Van Heusen Men t-shirt",
     price: 1819,
-    category: "T-shirt",
+    categories: ["t-shirt"],
   },
   {
     brand: "Fire voltt",
@@ -49,13 +75,35 @@ const products = [
     image: "https://m.media-amazon.com/images/I/61IMVMLW76L._SX679_.jpg",
     title: "Fire-Boltt Dazzle 1.83",
     price: 1049,
-    category: "Smartwatch",
+    categories: ["Smartwatch"],
   },
 ];
+
 async function main() {
-  await prisma.product.createMany({
-    data: products,
+  await prisma.category.createMany({
+    data: categories,
   });
+  await prisma.brand.createMany({
+    data: brands,
+  });
+
+  for await (const product of products) {
+    await prisma.product.create({
+      data: {
+        ...product,
+        brand: {
+          connect: {
+            name: product.brand,
+          },
+        },
+        categories: {
+          connect: product.categories.map((category) => ({
+            name: category,
+          })),
+        },
+      },
+    });
+  }
 }
 
 main()
